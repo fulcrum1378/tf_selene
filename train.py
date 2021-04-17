6695f3e3ef5667a0c99a235137d434c19a792ccb
Mahdi Parastesh <fulcrum1378@gmail.com>
2021-04-17 18:38:32 +0430

    2021.04.17 - 2

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from selene_sdk.utils import load_path
 from selene_sdk.utils import parse_configs_and_run
 
-configs = load_path("dataset/train.yaml")
+configs = load_path("training/config.yml")
 parse_configs_and_run(configs, lr=0.01)
